fac7df8c97568ff1c2aef83d102549c01ca566fc
Avionics <408431079@gms.tku.edu.tw>
2022-08-29 13:53:46 +0800

    W_L/W_TO change from 0.85 to 0.84

--- Boeing737MAX8_Performance_sizing/Performance_sizing_separate_plot.m
+++ Boeing737MAX8_Performance_sizing/Performance_sizing_separate_plot.m
@@ -103,7 +103,7 @@ hold on
 for CL_max_L = 1.8:0.2:2.4
     V_stall_sqrt = FieldLength/(0.3*1.3^2)/ft_s_to_kt^2;
     WoverS_landing = V_stall_sqrt/2*rho_FieldAltitude*CL_max_L;
-    WoverS_takeoff = WoverS_landing/0.85;
+    WoverS_takeoff = WoverS_landing/0.84;
     ToverW_landing = [0 1.6];
     WoverS_takeoff = [WoverS_takeoff WoverS_takeoff];
     plot(WoverS_takeoff,ToverW_landing);
