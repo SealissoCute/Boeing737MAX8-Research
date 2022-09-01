78fc9e7b1eee1daf941a068d7e5e567005096941
Avionics <408431079@gms.tku.edu.tw>
2022-09-01 16:42:46 +0800

    FieldLenght Update

--- Boeing737MAX8_Performance_sizing/Performance_sizing.m
+++ Boeing737MAX8_Performance_sizing/Performance_sizing.m
@@ -26,8 +26,9 @@ CruiseSpeed = CruiseSpeed_Mach*a_CruiseAltitude;
 q_overline = 0.5*rho_CruiseAltitude*CruiseSpeed^2;
 
 % Parameters at FieldAltitude RCTP FieldLength:12467ft/FieldAltitude:106ft
-FieldLength = 10000; % unit: ft
-FieldAltitude = 106; % unit: ft
+FieldLength_TO = 11000; % unit: ft
+FieldLength_L = 7400; % unit: ft
+FieldAltitude = 2000; % unit: ft
 [a,rho,P]=Standard_Atmosphere(FieldAltitude);
 rho_FieldAltitude = rho;
 P_FieldAltitude = P;
@@ -61,9 +62,10 @@ delta_CDO_Lflaps = 0.065;   % From p.127, Table 3.6
 delta_CD0_LG = 0.02;        % From p.127, Table 3.6CD_0_clean
 e_TOflaps = 0.8;            % From p.127, Table 3.6
 e_Lflaps = 0.75;            % From p.127, Table 3.6
+e_clean = 0.8;
 W_L = W_TO*0.84;            % 0.84 is from p.107, Table 3.3
 S = W_TO/100;               % W/S = 100
-CD_0_clean = f_2/S         % Take cf = 0.003
+CD_0_clean = f_2/S;         % Take cf = 0.003
 
 %
 % C_D0 = 0.0184; % p.145&182 low speed,clean drag polar
@@ -77,20 +79,20 @@ S_wiki = 1370; % unit: ft^2
 AR = b^2/S_wiki; % unit: ft
 S_wiki_TO = 1370; % unit: ft^2
 S_wet_wiki = 10^(c+d*log10(W_TO_wiki));
-StaticThrust_TO = 29317; % unit: lbs
+StaticThrust_TO = 26786; % unit: lbs
 WoverS_TO_wiki = W_TO_wiki/S_wiki_TO; % unit: lb/ft^2
 ToverW_TO_wiki = StaticThrust_TO*2/W_TO_wiki; % unit: lb/lb
 %%
 hold on
 % FAR25 TAKEOFF DISTANCE SIZING
 for CL_max_TO = 1.6:0.2:2.2
-    ToverW = 37.5/(Density_ratio_TO*CL_max_TO*FieldLength).*WoverS;
+    ToverW = 37.5/(Density_ratio_TO*CL_max_TO*FieldLength_TO).*WoverS;
     plot(WoverS,ToverW,'color',[0 0.4470 0.7410]); % blue
 end
 
 % FAR25 LANDING DISTANCE SIZING
 for CL_max_L = 1.8:0.2:2.4
-    V_stall_sqrt = FieldLength/(0.3*1.3^2)/ft_s_to_kt^2;
+    V_stall_sqrt = FieldLength_L/(0.3*1.3^2)/ft_s_to_kt^2;
     WoverS_landing = V_stall_sqrt/2*rho_FieldAltitude*CL_max_L;
     WoverS_takeoff = WoverS_landing/0.84;
     ToverW_landing = [0 1.6];
@@ -128,7 +130,7 @@ ToverW_TO3 = ToverW_TO/0.8;
 % FAR25.121 OEI
 CL_max = 1.4; % From Table 3.1
 CL = CL_max/1.25^2; % at 1.25 V_stall
-LoverD = CL/(CD_0_clean + CL^2/(pi*AR*0.85));
+LoverD = CL/(CD_0_clean + CL^2/(pi*AR*e_clean));
 ToverW_TO = 2*(1/LoverD+0.012); % CGR>0.012
 ToverW_TO4 = ToverW_TO/0.94/0.8; % 最大推力校正(除以0.94), 50°F效應(除以0.8)
 
